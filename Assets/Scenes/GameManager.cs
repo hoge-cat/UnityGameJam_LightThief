0cd1877c8915f950c8efed82598052d1ee6590aa
KikuchiTaiyo <ca01264013@st.kawahara.ac.jp>
2026-07-24 14:25:01 +0900

    ゲームオーバー判定の実装

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+//ステータス処理用のenum
+enum GameStatus
+{
+    Title,
+    Playing,
+    GameClear,
+    GameOver
+}
+
+public class gameR : MonoBehaviour
+{
+    //GameStatus型の変数
+    GameStatus gameStatus;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameStatus = GameStatus.Title;
+    }
+
+    public void GameOver()
+    {
+        gameStatus = GameStatus.GameOver;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayStatus();
+    }
+
+    //ステータス管理
+    void PlayStatus()
+    {
+        switch (gameStatus)
+        {
+            case GameStatus.Title:
+                break;
+
+            case GameStatus.Playing:
+                break;
+
+            case GameStatus.GameClear:
+                break;
+
+            case GameStatus.GameOver:
+                Debug.Log("ゲームオーバー");
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
