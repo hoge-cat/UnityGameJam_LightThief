aed16a13fbc3d834e185d384918c17cf36ca2197
Akiyama Ryuto <ca01264001@st.kawahara.ac.jp>
2026-07-24 13:55:49 +0900

    バッテリーのパーセントの表示を実装

--- Assets/Scripts/Manager/BatteryManager.cs
+++ Assets/Scripts/Manager/BatteryManager.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class BatteryManager : MonoBehaviour
 {
     [Header("Battery Settings")]
     public float maxBattery = 100f;
     public float currentBattery = 100f;
-
     public float drainSpeed = 1.0f;
-
     public bool flashlightOn = true;
-
+    public Slider batterySlider;
+    public TMP_Text batteryText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,17 +27,21 @@ public class BatteryManager : MonoBehaviour
             DrainBattery(drainSpeed * Time.deltaTime);
         }
 
+        batterySlider.value = currentBattery / maxBattery;
+
+        batteryText.text = "BATTERY\n" + Mathf.CeilToInt(currentBattery);
+
         // Tキーで30減らす（テスト）
-        //if (Keyboard.current.tKey.wasPressedThisFrame)
-        //{
-        //    DrainBattery(30f);
-        //}
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            DrainBattery(30f);
+        }
 
         //// Rキーで30回復（テスト）
-        //if (Keyboard.current.rKey.wasPressedThisFrame)
-        //{
-        //    AddBattery(30f);
-        //}
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            AddBattery(30f);
+        }
     }
 
     // バッテリーを減らす
