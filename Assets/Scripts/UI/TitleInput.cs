f54db8f657ad4a9e9a453c94a1d1c731f64fcb77
Oota Kouhei <ca01264006@st.kawahara.ac.jp>
2026-07-29 02:49:11 +0900

    タイトル画面に移動音と決定音を追加

--- Assets/Scripts/UI/TitleInput.cs
+++ Assets/Scripts/UI/TitleInput.cs
@@ -1,18 +1,75 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class TitleInput : MonoBehaviour
 {
+    [Header("シーン設定")]
     [SerializeField] private string mainSceneName = "Main";
+
+    [Header("メニュー音")]
+    [SerializeField] private TitleMenuSounds menuSounds;
+
+    [Header("遷移設定")]
+    [SerializeField] private float sceneLoadDelay = 0.2f;
+
+    private bool isProcessing;
 
     public void StartGame()
     {
+        if (isProcessing)
+        {
+            return;
+        }
+
+        StartCoroutine(StartGameCoroutine());
+    }
+
+    private IEnumerator StartGameCoroutine()
+    {
+        isProcessing = true;
+
+        menuSounds?.PlayDecide();
+
+        yield return new WaitForSecondsRealtime(
+            sceneLoadDelay
+        );
+
         Time.timeScale = 1.0f;
-        SceneManager.LoadScene(mainSceneName);
+
+        if (ScreenFader.Instance != null)
+        {
+            ScreenFader.Instance.LoadScene(
+                mainSceneName
+            );
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(
+                mainSceneName
+            );
+        }
     }
 
     public void QuitGame()
     {
+        if (isProcessing)
+        {
+            return;
+        }
+
+        StartCoroutine(QuitGameCoroutine());
+    }
+
+    private IEnumerator QuitGameCoroutine()
+    {
+        isProcessing = true;
+
+        menuSounds?.PlayDecide();
+
+        yield return new WaitForSecondsRealtime(
+            sceneLoadDelay
+        );
+
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
